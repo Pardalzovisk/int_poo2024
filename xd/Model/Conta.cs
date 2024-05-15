8e3c5bbe93fc5e3d1141886db80dbb86b84211e2
Enrico Pace <202210716@unifoa.edu.br>
2024-05-15 20:45:34 -0300

    nada

---
+++ xd/Model/Conta.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace xd.Model
+{
+    public class Conta
+    {
+        private long _numero;
+        private decimal _saldo;
+    }
+}
